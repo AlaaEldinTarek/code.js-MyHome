36786f51f9830456529a95f1cfaef222ec0fd736
Alaa Eldin Tarek <101481833+AlaaEldinTarek@users.noreply.github.com>
2022-12-17 23:16:12 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import Router from "next/router";
 import Head from "next/head";
-import NProgress from "nProgress";
+import NProgress from "nprogress";
 
 import "../styles/globals.css";
 import Layout from "../components/Layout";
