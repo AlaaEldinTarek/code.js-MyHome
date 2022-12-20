a8f11460b7031ec9d60c49d5382885741daaaa09
Alaa Eldin Tarek <101481833+AlaaEldinTarek@users.noreply.github.com>
2022-12-20 08:22:28 +0200

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,14 +1,18 @@
+import Link from "next/link";
+
 const Footer = () => {
   return (
     <div className="flex  justify-center items-center m-4   p-3 text-blue-400  border-blue-400 shadow-[-3px_-3px_10px_rgb(96_165_250_/_50),3px_3px_10px_rgb(96_165_250_/_50)]  shadow-blue-400 rounded-lg border-1">
       Copyright &copy;2022 All rights reserved by :&nbsp;
-      <a
-        href="https://www.al-coder.com/"
-        target="_blank"
-        className="text-yellow-100"
-      >
-        al-coder
-      </a>
+      <Link>
+        <a
+          href="https://www.al-coder.com/"
+          target="_blank"
+          className="text-yellow-100"
+        >
+          al-coder
+        </a>
+      </Link>
     </div>
   );
 };
